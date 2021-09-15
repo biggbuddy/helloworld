f32e0f1a07a58d13741ab5afc509d4f42bb8538c
chenjiming <chenjiming@jinyiyun.com>
2021-09-15 15:31:59 +0800

    添加xray配置

--- xray/xray_install.sh
+++ xray/xray_install.sh
@@ -407,7 +407,7 @@ function change_timezone(){
 }
 
 function down_config_files(){
-  wget -c -O web.confp https://raw.githubusercontent.com/biggbuddy/helloworld/master/xray/web.conf
+  wget -c -O web.conf https://raw.githubusercontent.com/biggbuddy/helloworld/master/xray/web.conf
   wget -c -O web.tar.gz https://raw.githubusercontent.com/biggbuddy/helloworld/master/xray/web.tar.gz
   wget -c -O xray_xtls-rprx-direct.json https://raw.githubusercontent.com/biggbuddy/helloworld/master/xray/xray_xtls-rprx-direct.json
 }
